194383dbf9b0b6292e77bdbb496675a15bad90ee
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-09-06 00:52:49 +0100

    ALX Interview — Star Wars API - Handle Errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,8 +6,14 @@ const request = require("request");
 // The first command-line argument passed is the Movie ID (e.g., 3 for "Return of the Jedi")
 const movieId = process.argv[2];
 
+// Check if movieId is not provided or is empty
+if (!movieId) {
+  console.error("Error: Movie ID must be provided as the first argument");
+  process.exit(1); // Exit the script
+}
+
 // URL for the Star Wars API with the movie ID
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 // Make a request to the Star Wars API to get movie details
 request(url, (error, response, body) => {
@@ -17,6 +23,12 @@ request(url, (error, response, body) => {
   } else {
     // Parse the response body as JSON to get movie data
     const filmData = JSON.parse(body);
+
+    // Check if filmData is not exist
+    if (filmData.detail === "Not found") {
+      console.error("Error: Movie Not found");
+      process.exit(1); // Exit the script
+    }
 
     // Extract the list of character URLs from the movie data
     const characters = filmData.characters;
